bf36a1e3eddbc9aea85190edec0198470f6c4b7d
Mohamad <mg.serag@gmail.com>
2024-06-21 20:04:55 -0700

    Add 1-last_digit.c to print the last digit of a random number with proper messages and update README.md

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,33 +6,34 @@
  * main - Entry point
  *
  * Description: Generates a random number, then prints the last digit
- * of the number and checks whether it is greater than 5, 0, or less than 6 and not 0.
+ * of the number and checks whether it is
+ * greater than 5, 0, or less than 6 and not 0.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  int n;
-  int last_digit;
+int n;
+int last_digit;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  last_digit = n % 10;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
 
-  printf("Last digit of %d is %d and is ", n, last_digit);
-  if (last_digit > 5)
-    {
-      printf("greater than 5\n");
-    }
-  else if (last_digit == 0)
-    {
-      printf("0\n");
-    }
-  else
-    {
-      printf("less than 6 and not 0\n");
-    }
+printf("Last digit of %d is %d and is ", n, last_digit);
+if (last_digit > 5)
+{
+printf("greater than 5\n");
+}
+else if (last_digit == 0)
+{
+printf("0\n");
+}
+else
+{
+printf("less than 6 and not 0\n");
+}
 
-  return (0);
+return (0);
 }
