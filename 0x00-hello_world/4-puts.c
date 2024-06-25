2f7c69074df64473954649a91386924eae3cd8b1
Mohamad <mg.serag@gmail.com>
2024-06-24 17:43:08 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,15 @@
 
 /**
  * put_string - Puts a string to the standard output
- * @str: The string to be printed
  *
  * Description: This function takes a string as input and prints
  * it to the standard output followed by a new line.
  *
  * Return: 0 (always success)
  */
-
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
