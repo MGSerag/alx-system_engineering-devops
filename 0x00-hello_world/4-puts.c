3d169c129e49f15835cc3ead4bdd11004c28d2f0
Mohamad <mg.serag@gmail.com>
2024-06-24 17:28:09 -0700

    Add 2-assembler, 3-name, 4-puts.c, 100-intel, 101-quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+/**
+ * put_string - Puts a string to the standard output
+ * @str: The string to be printed
+ *
+ * Description: This function takes a string as input and prints
+ * it to the standard output followed by a new line.
+ *
+ * Return: 0 (always success)
+ */
+
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
 
