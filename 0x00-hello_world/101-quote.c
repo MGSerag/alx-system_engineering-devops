3f14cad06b517341b299683208cb690161b558b6
Mohamad <mg.serag@gmail.com>
2024-06-24 17:38:56 -0700

    Update 101-quote.c to concatenate two string parts for the message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\"- Dora Korpar,"
-"2015-1-19\n";
+    const char *message_part1 = "and that piece of art is useful\" - Dora Korpar,";
+    const char *message_part2 = " 2015-10-19\n";
+    const char *message = message_part1 + message_part2;
 
-write(2, message, 59);
-return (1);
+    write(2, message, 59);
+    return (1);
 }
-
